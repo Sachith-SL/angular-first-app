ec6eb87232cacb146472a055fe9ec6724b6965c5
Sachith-SL <itsmesachith@gmail.com>
2025-07-26 08:01:45 +0530

    Event listener in Angular

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { GreetingComponent } from '../components/greeting/greeting.component';
+import { CounterComponent } from '../components/counter/counter.component';
 
 @Component({
   selector: 'app-home',
-  imports: [GreetingComponent],
+  imports: [GreetingComponent,CounterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   message = signal('come from Home component variable');
+  keyUpHandler(event:KeyboardEvent) {
+    console.log(`you press the {${event.key}} key`);
+  }
 }
